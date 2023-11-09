a03e024a2256a993c2b8c2ec40132d524ce27d20
Irock <sebastianlarabarria@gmail.com>
2023-11-08 21:22:10 -0300

    Funciones y botones para encender,apagar y actualizar el bot. Con esto se pueden usar los comandos en Telegram

--- Front/botTelegram/screens/CommandScreen.js
+++ Front/botTelegram/screens/CommandScreen.js
@@ -26,6 +26,7 @@ const CommandScreen = () => {
             const data = await response.json();
             console.log(data);
             Alert.alert("Comando enviado con éxito");
+            getCommands();
             setCommand('');
             setNameCommand('');
         } catch (error) {
@@ -39,6 +40,7 @@ const CommandScreen = () => {
       try {
         const response = await fetch('http://192.168.0.13:3000/api/command');
         const jsonResponse = await response.json();
+        console.log(jsonResponse);
         if (jsonResponse.ok === "success" && Array.isArray(jsonResponse.data)) {
           setCommandsList(jsonResponse.data);
           Alert.alert("Comandos obtenidos con éxito");
@@ -51,6 +53,101 @@ const CommandScreen = () => {
         Alert.alert("Error al obtener comandos", error.toString());
       }
     };
+
+    const stopBot = async () => {
+      try {
+        const response = await fetch('http://192.168.0.13:3000/api/bot/stop', {
+          method: 'GET',
+        });
+        const jsonResponse = await response.json();
+        if (jsonResponse.ok === "success") {
+          Alert.alert("Bot detenido con éxito");
+        } else {
+          console.error('Error al detener el bot:', jsonResponse.error);
+          // Si el bot no estaba encendido, aún queremos actualizarlo con los nuevos comandos.
+          if (jsonResponse.error === "El bot no ha sido encendido") {
+            Alert.alert("El bot no estaba encendido. Actualizando...");
+            startBot(commandsList);
+          } else {
+            // Manejar otros posibles errores
+            Alert.alert("Otro error...");
+          }
+        }
+      } catch (error) {
+        console.error('Error al detener el bot:', error);
+        Alert.alert("Error al detener el bot", error.toString());
+      }
+    };
+    
+    
+    const startBot = async () => {
+      try {
+        // Obtener los datos de los comandos para el bot
+        const commandDataResponse = await fetch('http://192.168.0.13:3000/api/command');
+        const commandData = await commandDataResponse.json();
+        
+        // Asegúrate de que la respuesta contiene la propiedad 'data'
+        if (!commandData || !commandData.data) {
+          console.error('La respuesta no contiene la propiedad "data" esperada.');
+          return;
+        }
+    
+        // Los 'interactions' deben ser un array, que es lo que se espera en el servidor.
+        const interactions = commandData.data;
+    
+        // Iniciar el bot con las interacciones obtenidas
+        let startResponse = await fetch('http://192.168.0.13:3000/api/bot/start', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ interactions }), // Asegúrate de pasar las interacciones como un array
+        });
+        
+        let jsonResponse = await startResponse.json();
+        
+        // Manejar la respuesta del servidor después de intentar iniciar el bot
+        if (jsonResponse.ok === "success") {
+          Alert.alert("Bot iniciado con éxito");
+        } else {
+          console.error('Error al iniciar el bot:', jsonResponse);
+          Alert.alert("Error al iniciar el bot", jsonResponse.message || 'Error desconocido');
+        }
+      } catch (error) {
+        console.error('Error al iniciar el bot:', error);
+        Alert.alert("Error al iniciar el bot", error.toString());
+      }
+    };
+    
+
+    const updateBot = async (interactions) => {
+      try {
+          const response = await fetch('http://192.168.0.13:3000/api/bot/update', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ interactions }),
+          });
+          const jsonResponse = await response.json();
+          if (jsonResponse.ok === "success") {
+              Alert.alert("Bot actualizado con éxito","Ya puedes usar los comandos en Telegram");
+          } else {
+              console.error('Error al actualizar el bot:', jsonResponse);
+              Alert.alert("Error al actualizar el bot", jsonResponse.error);
+          }
+      } catch (error) {
+          console.error('Error al actualizar el bot:', error);
+          Alert.alert("Error al actualizar el bot", error.toString());
+      }
+  };
+
+  const getAndUpdateCommands = async () => {
+    await getCommands();
+    await updateBot(commandsList);
+};
+
+    
     
   
 
@@ -93,6 +190,28 @@ const CommandScreen = () => {
                         <Text style={styles.commandText}>{item.command}: {item.message}</Text>
                     </View>
                 ))}
+                <Button
+                    height={40}
+                    width={'40%'}
+                    onPress={startBot}
+                    title="Encender bot"
+                    buttonColor={'#00802C'}
+                />
+                <Button
+                    height={40}
+                    width={'40%'}
+                    onPress={stopBot}
+                    title="Apagar bot"
+                    buttonColor={'#BD0000'}
+                    MRight={10}
+                />
+                <Button
+                    height={40}
+                    width={'40%'}
+                    onPress={getAndUpdateCommands}
+                    title="Actualizar bot"
+                    buttonColor={'#b03084'}
+                />
             </View>
         </View>
     );
